6d7f43a80eb82f02e033e245a757c04ff8dfcc8d
yuyz <12637700@qq.com>
2020-04-08 23:21:05 +0800

    update player & add anim bp

--- Source/ShadowSong/Private/SSCharacterBase.cpp
+++ Source/ShadowSong/Private/SSCharacterBase.cpp
@@ -2,8 +2,10 @@
 
 
 #include "SSCharacterBase.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "AbilitySystemComponent.h"
-#include "..\Public\SSCharacterBase.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 ASSCharacterBase::ASSCharacterBase()
@@ -11,13 +13,56 @@ ASSCharacterBase::ASSCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Face = CreateMeshPart<USkeletalMeshComponent>(TEXT("Face"));
+	Hair = CreateMeshPart<USkeletalMeshComponent>(TEXT("Hair"));
+	Glove = CreateMeshPart<USkeletalMeshComponent>(TEXT("Glove"));
+	Shoe = CreateMeshPart<USkeletalMeshComponent>(TEXT("Shoe"));
+	Shoulder = CreateMeshPart<USkeletalMeshComponent>(TEXT("Shoulder"));
+	Helm = CreateMeshPart<USkeletalMeshComponent>(TEXT("Helm"));
+	Belt = CreateMeshPart<USkeletalMeshComponent>(TEXT("Belt"));
+
+	Shield = CreateMeshPart<UStaticMeshComponent>(TEXT("Shield"), "hand_l");
+	LeftWeapon = CreateMeshPart<UStaticMeshComponent>(TEXT("LeftWeapon"), "hand_lSocket");
+	RightWeapon = CreateMeshPart<UStaticMeshComponent>(TEXT("RightWeapon"), "hand_rSocket");
+	Backpack = CreateMeshPart<UStaticMeshComponent>(TEXT("Backpack"), "Backpack");
 }
 
 // Called when the game starts or when spawned
 void ASSCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	for (EPartType pt = EPartType::PT_Cloth; pt < EPartType::PT_Backpack;)
+	{
+		SelectedPart.Add(pt, TEXT("NONE"));
+		uint8 tmp = (uint8)pt;
+		tmp++;
+		pt = (EPartType)tmp;
+	}
+
+	USkeletalMeshComponent* master = this->GetMesh();
+	CharacterMeshComponents.Add(TEXT("Cloth"), master);
+	TArray<USceneComponent*> children;
+	master->GetChildrenComponents(false, children);
+	for (int i = 0; i < children.Num(); i++)
+	{
+		USceneComponent* comp = children[i];
+		FString name = UKismetSystemLibrary::GetObjectName(comp);
+		UMeshComponent* mesh = Cast<UMeshComponent>(comp);
+		if (mesh)
+		{
+			CharacterMeshComponents.Add(name, mesh);
+		}
+	}
 	
+	for (auto& it : CharacterMeshComponents)
+	{
+		USkeletalMeshComponent* skeletal = Cast<USkeletalMeshComponent>(it.Value);
+		if (skeletal != nullptr)
+		{
+			skeletal->SetMasterPoseComponent(master);
+		}
+	}
 }
 
 // Called every frame
--- Source/ShadowSong/Public/SSCharacterBase.h
+++ Source/ShadowSong/Public/SSCharacterBase.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
+#include "SSTypes.h"
 #include "SSCharacterBase.generated.h"
 
 class UAbilitySystemComponent;
@@ -14,6 +15,36 @@ class SHADOWSONG_API ASSCharacterBase : public ACharacter, public IAbilitySystem
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Face;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Hair;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Glove;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Shoe;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Shoulder;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Helm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BaseParts")
+	class USkeletalMeshComponent* Belt;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ExtraParts")
+	class UStaticMeshComponent* Shield;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ExtraParts")
+	class UStaticMeshComponent* LeftWeapon;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ExtraParts")
+	class UStaticMeshComponent* RightWeapon;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ExtraParts")
+	class UStaticMeshComponent* Backpack;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Select")
+	TMap<FString, class UMeshComponent*> CharacterMeshComponents;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Select")
+	TMap<EPartType, FString> SelectedPart;
+
 public:
 	// Sets default values for this character's properties
 	ASSCharacterBase();
@@ -21,6 +52,15 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	template <class T>
+	T* CreateMeshPart(FString name, FName socket = NAME_None)
+	{
+		auto part = CreateDefaultSubobject<T>(FName(*name));
+		part->SetupAttachment(this->GetMesh(), socket);
+		CharacterMeshComponents.Add(name, part);
+		return part;
+	}
 
 public:	
 	// Called every frame
