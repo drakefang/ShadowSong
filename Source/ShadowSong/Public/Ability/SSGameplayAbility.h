4148c2031af1b60209650474ed3bf01553a36d40
drakefang <nomad.ccnu@gmail.com>
2020-05-07 20:27:45 +0800

    gameplay effect

--- Source/ShadowSong/Public/Ability/SSGameplayAbility.h
+++ Source/ShadowSong/Public/Ability/SSGameplayAbility.h
@@ -7,6 +7,20 @@
 #include "Abilities/GameplayAbility.h"
 #include "SSGameplayAbility.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FSSGamePlayEffectContainer
+{
+	GENERATED_BODY()
+
+public:
+	FSSGamePlayEffectContainer() {}
+
+	
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Effect Container")
+	TArray<TSubclassOf<UGameplayEffect>> GameplayEffects;
+};
+
 /**
  * 
  */
@@ -26,6 +40,9 @@ public:
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Ability")
 	bool bActivateAbilityOnGranted = false;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Ability")
+	TMap<FGameplayTag, FSSGamePlayEffectContainer> EffectMap;
 
 	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
 };
